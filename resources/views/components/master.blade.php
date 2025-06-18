184bbb54985075f601f91926c664fe13bec8683f
Zakaria Senbati <zakisenbati.git@gmail.com>
2025-06-18 15:29:23 +0100

    Vite

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -6,11 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
-/>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>GOAT | {{$title}}</title>
 </head>
+
 <body>
     @include('partials.nav')
 
@@ -30,8 +29,6 @@
     @include('partials.footer')
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
 
-    </script>
 </body>
 </html>
